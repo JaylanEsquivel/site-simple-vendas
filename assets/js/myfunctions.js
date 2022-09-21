9d6ff90b41572ca9241c55fc51993634ab30a5c8
JaylanEsquivel <jaylan1997@hotmail.com>
2022-09-21 20:31:42 -0300

    navbar scroll

--- assets/js/myfunctions.js
+++ assets/js/myfunctions.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-   
+
+    window.onscroll = function() {scrollFunction()};
+
 });
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+
+
+    $(".navbar").css("position","fixed");
+    $(".navbar").css("width","100%");
+
+
+  } else {
+    $(".navbar").css("position","relative");
+    $(".navbar").css("width","100%");
+  }
+}
+
